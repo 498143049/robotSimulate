ea720ceee6e6f3bbf48d67cf602914661fd78ee4
John Doe <498143049@qq.com>
2016-04-04 19:55:16 +0800

    it can get joint num and joints

--- loadData.m
+++ loadData.m
@@ -11,8 +11,16 @@ function data = loadData(path)
 	 fclose(fid);
 	 data = p_json(inString);
 	 Num=length(data.robot.link);
+	 ActionjointNum=0;
+	 Actionjoint=[];
 	 for i=1: Num
 		[data.robot.link{1,i}.v,data.robot.link{1,i}.f,data.robot.link{1,i}.Num]=stlread(['data/',strtrim(data.robot.link{1,i}.fileName)]);
 		[data.robot.link{1,i}.v]=[data.robot.link{1,i}.v,ones(data.robot.link{1,i}.Num*3,1)];   %给其增加一列使其变为向量
+		if ~strcmpi(data.robot.link{1, i}.type,'base')
+			ActionjointNum=ActionjointNum+1;	
+			Actionjoint=[Actionjoint,i];
+		end
 	 end
+	 data.robot.ActionjointNum=ActionjointNum;
+	 data.robot.Actionjoint=Actionjoint;
 
