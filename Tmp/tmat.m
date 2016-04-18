462e599e6372b3e23a9e36a3a3e3b42f4bb9d95f
John Doe <498143049@qq.com>
2016-04-18 23:56:06 +0800

    up

--- Tmp/tmat.m
+++
@@ -1,7 +0,0 @@
- function T = tmat(Dhparametes)
-  c = cos(Dhparametes.theta);
-  s = sin(Dhparametes.theta);
-  ca = cosd(Dhparametes.alpha);
-  sa = sind(Dhparametes.alpha);
-  T = [c -s 0 Dhparametes.A; s*ca c*ca -sa -sa*Dhparametes.D; s*sa c*sa ca ca*Dhparametes.D; 0 0 0 1]; 
-end
